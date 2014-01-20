d281c1e68e23f9f9ea43e89756483d3f778e7263
Arnaud Berthomier <oz@cyprio.net>
2014-01-20 12:59:13 -0600

    Initial import

---
+++ about.md
@@ -0,0 +1,30 @@
+---
+title: About
+---
+
+# Well, what about this place?
+
+I have never been very good at maintaining websites, but since I do feel
+the urge to write sometimes (sorry about that), I have no other choice
+than to keep this website online.
+
+Furthermore, this site helps me a lot to keep in touch with family and
+friends.  As I have been living away from most of them for quite a
+while, and not all can afford to travel to Mexico.
+
+As a side note, I want to ensure you that no animals were harmed in the
+making of this website.
+
+# The author
+
+Now that I made clear that most of the content is not interesting to the
+general public but a select few, here are a few notes on myself, and the
+various online places where I can be found.
+
+ * I tweet as [ephoz][twitter],
+ * I archive and publish some pictures on Flickr, as [ephoz][flickr],
+ * and I push code on Github as [github][github].
+
+[twitter]: https://twitter.com/ephoz
+[flickr]: https://flickr.com/photos/ephoz
+[github]: https://github.com/oz
---
+++ site.hs
@@ -0,0 +1,126 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend, mconcat)
+import           Hakyll
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "img/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    -- Copy regular files (HTML5 Boilerplate CSS, favicon, some JS...)
+    match (fromList ["favicon.ico", "robots.txt"])$ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "img/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*.css" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    -- Compile Sass styles
+    match "css/main.scss" $ do
+        route   $ setExtension "css"
+        compile $ getResourceString >>= sassify
+
+    match (fromList ["about.md", "contact.md"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "wtf/*.md"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "wtf/*.md"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+-------------------------------------------------------------------------------
+-- Bloggy thing
+    match "wtf/*.md" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+-- RSS feed
+    create ["wtf/index.xml"] $ do
+            route idRoute
+            compile $ do
+                    loadAllSnapshots "wtf/*.md" "content"
+                    >>= fmap (take 10) . recentFirst
+                    >>= renderRss wtfFeedConfig feedCtx
+
+-- Atom feed
+    create ["wtf/atom.xml"] $ do
+            route idRoute
+            compile $ do
+                    loadAllSnapshots "wtf/*.md" "content"
+                    >>= fmap (take 10) . recentFirst
+                    >>= renderAtom wtfFeedConfig feedCtx
+-------------------------------------------------------------------------------
+-- Feeds stuff
+feedCtx :: Context String
+feedCtx = mconcat
+        [ bodyField "description"
+        , defaultContext
+        ]
+
+wtfFeedConfig :: FeedConfiguration
+wtfFeedConfig = feedConfig " - Latest wtf posts"
+
+feedConfig :: String -> FeedConfiguration
+feedConfig title = FeedConfiguration
+        { feedTitle       = "Mostly harmless" ++ title
+        , feedDescription = "rants and moods from Mexico"
+        , feedAuthorName  = "oz"
+        , feedAuthorEmail = "oz@cyprio.net"
+        , feedRoot        = "http://cyprio.net"
+        }
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+--------------------------------------------------------------------------------
+sassify :: Item String -> Compiler (Item String)
+sassify item = withItemBody (unixFilter "sass" ["-s", "--scss", "--load-path", "css"]) item
+               >>= return . fmap compressCss
